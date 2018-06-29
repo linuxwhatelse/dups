7f668a8a90d571231f3a7288b921933e0f8f4504
tadly <git@tadly.de>
2018-06-29 11:06:34 +0200

    improves `backup.Backup`_ comp functions

--- dups/backup.py
+++ dups/backup.py
@@ -146,21 +146,33 @@ class Backup(object):
         return self.name
 
     def __lt__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name < other.name
 
     def __gt__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name > other.name
 
     def __eq__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name == other.name
 
     def __le__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name <= other.name
 
     def __ge__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name >= other.name
 
     def __ne__(self, other):
+        if not isinstance(other, Backup):
+            return False
         return self.name != other.name
 
     @property
