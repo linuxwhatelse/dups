639e11f55d4b64fa7339ba8113039bec7d319861
tadly <git@tadly.de>
2018-08-20 20:22:19 +0200

    fixes daemon not properly using create-/restore_backup

--- dups/daemon.py
+++ dups/daemon.py
@@ -108,7 +108,8 @@ class Daemon(dbus.service.Object):
             config.Config.get().reload()
 
             self._notify('Starting new backup')
-            success, res = helper.error_handler(helper.create_backup, dry_run)
+            success, res = helper.error_handler(helper.create_backup, self.usr,
+                                                dry_run)
 
             if success:
                 bak, status = res
@@ -138,8 +139,8 @@ class Daemon(dbus.service.Object):
             config.Config.get().reload()
 
             self._notify('Starting restore')
-            success, res = helper.error_handler(helper.restore_backup, items,
-                                                name, target, dry_run)
+            success, res = helper.error_handler(
+                helper.restore_backup, self.usr, items, name, target, dry_run)
             if success:
                 bak, status = res
                 self._notify('Finished restore', status.message)
