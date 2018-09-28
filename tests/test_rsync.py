b259aada560f4dca58a1bd72dd7bbb26cbd8f807
tadly <git@tadly.de>
2018-09-28 12:05:06 +0200

    adds some initial "new style" unittests for rsync

--- tests/test_rsync.py
+++ tests/test_rsync.py
@@ -1,7 +1,9 @@
 import context  # noqa: F401, isort:skip
 
 import os
+import shlex
 import shutil
+from unittest.mock import patch
 
 from dups import const, rsync
 
@@ -46,6 +48,94 @@ class Test_Status:
 
 
 class Test_rsync:
+    __start_dir = os.getcwd()
+
+    def setup_method(self, method):
+        os.makedirs(context.TMP_DIR)
+        os.chdir(context.TMP_DIR)
+
+    def teardown_method(self, method):
+        os.chdir(self.__start_dir)
+        if os.path.isdir(context.TMP_DIR):
+            shutil.rmtree(context.TMP_DIR)
+
+    @patch('dups.rsync.rsync._exec')
+    def test_options_original(self, mock_exec):
+        sync = rsync.rsync()
+        sync.sync(rsync.Path('/'), [])
+
+        cmd = mock_exec.call_args[0][0]
+        assert b'--acls' in cmd
+        assert b'--xattrs' in cmd
+        assert b'--prune-empty-dirs' in cmd
+        assert b"--out-format '%t %i %n'" in cmd
+        assert b'--dry-run' in cmd
+
+    @patch('dups.rsync.rsync._exec')
+    def test_options_modified(self, mock_exec):
+        sync = rsync.rsync()
+
+        sync.acls = False
+        sync.xattrs = False
+        sync.prune_empty_dirs = False
+        sync.out_format = "%t %i %f %''b"
+        sync.dry_run = False
+
+        sync.sync(rsync.Path('/'), [])
+
+        cmd = mock_exec.call_args[0][0]
+        assert b'--acls' not in cmd
+        assert b'--xattrs' not in cmd
+        assert b'--prune-empty-dirs' not in cmd
+        assert '--out-format {}'.format(
+            shlex.quote("%t %i %f %''b")).encode() in cmd
+        assert b'--dry-run' not in cmd
+
+    @patch('dups.rsync.rsync._exec')
+    def test_includes(self, mock_exec):
+        test_utils.create_dir_struct({
+            'simple.file': None,
+            'simple folder': {},
+            r'special * folder': {},
+            r'''!"#$%&'()*+,-.012:;<=>?@ABC[\]^_`abc{|}~''': None
+        })
+
+        sync = rsync.rsync()
+        sync.sync(
+            rsync.Path(context.TMP_DIR), [
+                'simple.file',
+                'simple folder',
+                'special * folder',
+                'not found * folder',
+                r'test\ \*\ folder/*.pattern',
+                r'''!"#$%&'()*+,-.012:;<=>?@ABC[\]^_`abc{|}~''',
+            ])
+
+        cmd = cmd = mock_exec.call_args[0][0]
+        assert b'simple.file' in cmd
+        assert b"'simple folder'" in cmd
+        assert b"'special * folder'" in cmd
+        assert b'not found * folder' in cmd
+        assert rb'test\ \*\ folder/*.pattern' in cmd
+        assert b"'!"  #$%&' "'" '()*+,-.012:;<=>?@ABC[\]^_`abc{|}~'" in cmd
+
+    @patch('dups.rsync.rsync._exec')
+    def test_excludes(self, mock_exec):
+        sync = rsync.rsync()
+        sync.sync(
+            rsync.Path(context.TMP_DIR), ['/'], [
+                '/tmp',
+                'simple folder',
+                '*.mkv',
+            ])
+
+        cmd = mock_exec.call_args[0][0]
+        assert b"--exclude /tmp" in cmd
+        assert b"--exclude 'simple folder'" in cmd
+        assert b"--exclude '*.mkv'" in cmd
+
+
+class Test_rsync_old:
     @property
     def data_dir_struct(self):
         return test_utils.get_dir_struct(context.DATA_DIR)
@@ -108,24 +198,6 @@ class Test_rsync:
 
         # Send the files
         target = rsync.Path(context.TMP_DIR, context.SSH_HOST)
-        sync.sync(target, [context.TEST_DIR, context.TEST_FILE],
-                  excludes=['**/dir2/.gitkeep'])
-
-        # Get and compare the structure of our sync target
-        synced_data = test_utils.get_dir_struct(self.real_target)
-        assert expected_data == synced_data
-
-    def test_options(self):
-        sync = rsync.rsync()
-        sync.dry_run = False
-        sync.prune_empty_dirs = False
-
-        # Define the structure we expect after synchronizing
-        expected_data = self.data_dir_struct
-        del expected_data['test.dir']['dir2']['.gitkeep']
-
-        # Send the files
-        target = rsync.Path(context.TMP_DIR)
         sync.sync(target, [context.TEST_DIR, context.TEST_FILE],
                   excludes=['**/dir2/.gitkeep'])
 
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,19 @@
 import os
 
 
+def create_dir_struct(structure, target='.'):
+    for name, item in structure.items():
+        path = os.path.join(os.path.abspath(target).encode(), name.encode())
+
+        if item is None:
+            open(path, 'a').close()
+
+        elif isinstance(item, dict):
+            os.makedirs(path)
+            if len(item) > 0:
+                create_dir_struct(path.decode(), item)
+
+
 def get_dir_struct(target):
     struct = {}
     for item in os.listdir(target):
